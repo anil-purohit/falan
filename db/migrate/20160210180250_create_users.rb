3f7d592141bf929ba4c24d5f13b01fa77d7384ad
a-nil <anilpurohit711@gmail.com>
2016-03-02 19:50:27 +0530

    added users

---
+++ db/migrate/20160210180250_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+
+      t.string :email_id, :null => false
+      t.string :first_name
+      t.string :last_name
+      t.integer :status, :default => 0
+      t.integer :signup_medium, :default => 0
+      t.string :signup_id
+      t.timestamps :null => false
+    end
+  end
+end
