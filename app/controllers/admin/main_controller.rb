e90e8240986459cb43757ef8dd2b86685fe53fcf
a-nil <anilpurohit711@gmail.com>
2016-06-03 23:04:56 +0530

    added password

--- app/controllers/admin/main_controller.rb
+++ app/controllers/admin/main_controller.rb
@@ -1,2 +1,10 @@
 class Admin::MainController < ApplicationController
+  before_action :validate_admin_password
+
+  private
+  def validate_admin_password
+    if params[:password] != "obsAdmin"
+      redirect_to root_path
+    end
+  end
 end
