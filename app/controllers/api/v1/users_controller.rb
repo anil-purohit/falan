3b1fa0ac50a23750bdc4d698d4e9f75ef2aec252
a-nil <anilpurohit711@gmail.com>
2016-06-04 11:41:09 +0530

    added location update

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::UsersController < Api::MainController
     id = params[:id].to_i
     create_response(400, "Invalid User Id.") if id <=0
 
-    user = User.where(:id => params[:id])
+    user = User.where(:id => params[:id]).first
     if user.present?
       create_response(200, user)
     else
@@ -40,9 +40,25 @@ class Api::V1::UsersController < Api::MainController
     result ? create_response(200, user) : create_response(400, user.errors)
   end
 
+  def update_user_location
+    id = params[:id].to_i
+    return create_response(400, "Invalid User Id.") if id <=0 || params[:location].blank?
+    user = User.where(:id => id).first
+    location_info = Geokit::Geocoders::GoogleGeocoder.geocode params[:location].to_s
+    if location_info.present?
+      lat_long = location_info.ll
+      user[:lat] = lat_long.split(",")[0]
+      user[:long] = lat_long.split(",")[1]
+    else
+      add_default_location user
+    end
+    result = user.save!
+    result ? create_response(200, user) : create_response(400, user.errors)
+  end
+
   def add_default_location(user)
-    user[:location] = "Bangalore"
-    user[:lat] = 12.9716
-    user[:long] = 77.5946
+    user[:location] = "Delhi"
+    user[:lat] = 28.6139
+    user[:long] = 77.2090
   end
 end
