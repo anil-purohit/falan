12afa0fe67faed32517dfe81d8d639486027bed7
a-nil <anilpurohit711@gmail.com>
2016-07-25 14:03:50 +0530

    added book id

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,7 @@
 class Book < ActiveRecord::Base
   def self.to_hash(book)
     book_info = {}
+    book_info[:book_id] = book[:id]
     book_info[:title] = book[:title]
     book_info[:author] = book[:author]
     book_info[:publication]= book[:publication]
